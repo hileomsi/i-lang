64cd1cd11923b222e8fe58f9c2918577a35407b7
Hiléo Andersson <hileomsi@gmail.com>
2019-06-12 01:59:34 -0300

    fix: eslint,reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,103 +1,117 @@
 module.exports = {
-  parser: "babel-eslint",
-  env: {
-    "browser": true,
-    "jest": true
-  },
-  parserOptions: {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
-    }
-  },
-  plugins: [
-    "react-extra",
-    "jsx-a11y",
-    "import"
-  ],
-  extends: "airbnb",
-  rules: {
-    "indent": [ "error", 2 ],
-    "linebreak-style": [ "error", "unix" ],
-    "quotes": [ "error", "single" ],
-    "semi": [ "error", "always" ],
-    "react/jsx-no-undef": [ 2, {
-      "allowGlobals": true
-    } ],
-    "arrow-parens": [ "error", "as-needed" ],
-    "comma-dangle": [ "error", "never" ],
-    "global-require": "off",
-    "import/first": "off",
-    "import/no-duplicates": "off",
-    "import/no-named-as-default": "off",
-    "import/no-extraneous-dependencies": [ "error", {
-      "devDependencies": true
-    } ],
-    "import/order": "off",
-    "import/prefer-default-export": "off",
-    "jsx-a11y/anchor-is-valid": "off",
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/label-has-for": "off",
-    "jsx-control-statements/jsx-jcs-no-undef": "off",
-    "jsx-quotes": [ "error", "prefer-single" ],
-    "lines-between-class-members": [ "error", "always", {
-      "exceptAfterSingleLine": true
-    } ],
-    "no-confusing-arrow": "off",
-    "no-console": "off",
-    "no-underscore-dangle": "off",
-    "no-use-before-define": "off",
-    "object-curly-newline": "off",
-    "react/destructuring-assignment": [ true, "always", {
-      "ignoreClassFields": true
-    } ],
-    "react/no-typos": "off",
-    "react/jsx-filename-extension": [ "error", {
-      "extensions": [ ".js", ".jsx" ]
-    } ],
-    "react-extra/no-inline-styles": 2,
-    "keyword-spacing": [
-      "error",
-      {
-        "before": true,
-        "after": false,
-        "overrides": {
-          "import": {
-            "after": true
-          },
-          "from": {
-            "after": true
-          },
-          "export": {
-            "after": true
-          },
-          "const": {
-            "after": true
-          },
-          "let": {
-            "after": true
-          },
-          "else": {
-            "after": true
-          },
-          "return": {
-            "after": true
-          },
-          "try": {
-            "after": true
-          },
-          "finally": {
-            "after": true
-          }
-        }
-      }
-    ]
-  },
-  settings: {
-    "import/resolver": {
-      "babel-module": {
-        "root": [ "./src" ]
-      }
-    }
-  }
+	parser: 'babel-eslint',
+	env: {
+		browser: true,
+		jest: true,
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			experimentalObjectRestSpread: true,
+			jsx: true,
+		},
+	},
+	plugins: ['react-extra', 'jsx-a11y', 'import', 'flowtype'],
+	extends: ['airbnb', 'plugin:flowtype/recommended'],
+	rules: {
+		indent: ['error', 2],
+		'no-tabs': 0,
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'react/jsx-no-undef': [
+			2,
+			{
+				allowGlobals: true,
+			},
+		],
+		allowIndentationTabs: true,
+		'arrow-parens': ['error', 'as-needed'],
+		'comma-dangle': ['error', 'never'],
+		'global-require': 'off',
+		'import/first': 'off',
+		'import/no-duplicates': 'off',
+		'import/no-named-as-default': 'off',
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				devDependencies: true,
+			},
+		],
+		'import/order': 'off',
+		'import/prefer-default-export': 'off',
+		'jsx-a11y/anchor-is-valid': 'off',
+		'jsx-a11y/href-no-hash': 'off',
+		'jsx-a11y/label-has-for': 'off',
+		'jsx-quotes': ['error', 'prefer-single'],
+		'lines-between-class-members': [
+			'error',
+			'always',
+			{
+				exceptAfterSingleLine: true,
+			},
+		],
+		'no-confusing-arrow': 'off',
+		'no-console': 'off',
+		'no-underscore-dangle': 'off',
+		'no-use-before-define': 'off',
+		'object-curly-newline': 'off',
+		'react/destructuring-assignment': [
+			true,
+			'always',
+			{
+				ignoreClassFields: true,
+			},
+		],
+		'react/no-typos': 'off',
+		'react/jsx-filename-extension': [
+			'error',
+			{
+				extensions: ['.js', '.jsx'],
+			},
+		],
+		'react-extra/no-inline-styles': 2,
+		'keyword-spacing': [
+			'error',
+			{
+				before: true,
+				after: false,
+				overrides: {
+					import: {
+						after: true,
+					},
+					from: {
+						after: true,
+					},
+					export: {
+						after: true,
+					},
+					const: {
+						after: true,
+					},
+					let: {
+						after: true,
+					},
+					else: {
+						after: true,
+					},
+					return: {
+						after: true,
+					},
+					try: {
+						after: true,
+					},
+					finally: {
+						after: true,
+					},
+				},
+			},
+		],
+	},
+	settings: {
+		'import/resolver': {
+			'babel-module': {
+				root: ['./src'],
+			},
+		},
+	},
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import 'config/ReactotronConfig';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './scenes/Login';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
